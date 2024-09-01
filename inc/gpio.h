661cff41be3915b7aad5bad20b401839da910dd2
Micaeldm <martinutn30@gmail.com>
2024-09-01 16:56:11 -0300

    cambios en gpio.h

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,13 +1,45 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * @brief declaro un puntero a struct anonima porque no declaro los mienbros
+ * 
+ */
+typedef struct gpio_s * gpio_t; 
 
-typedef struct gpio_s * gpio_t; // declaro un puntero astruct anonima porque no declaro los mienbros
+/**
+ * @brief funcion para crear el objeto
+ * 
+ * @param puerto 
+ * @param bit 
+ * @return gpio_t 
+ */
 
-gpio_t gpioCreate(uint8_t puerto, uint8_t bit); //para crear el objeto y recibe las elementar para que funcione o no recibir nada y me devuelve el puntero gpio_t
+gpio_t gpioCreate(uint8_t puerto, uint8_t bit); 
+
+/**
+ * @brief funcion para setear si es salida o entrada
+ * 
+ * @param gpio 
+ * @param output 
+ */
 
 void gpioSetOutput(gpio_t gpio, bool output);
 
+/**
+ * @brief Funcion para saber el estado
+ * 
+ * @param gpio 
+ * @param state 
+ */
 void gpioSetState(gpio_t gpio, bool state);
+
+/**
+ * @brief Funcion para setear el estado ON/OFF
+ * 
+ * @param gpio 
+ * @return true 
+ * @return false 
+ */
 
 bool gpioGetState(gpio_t gpio);
